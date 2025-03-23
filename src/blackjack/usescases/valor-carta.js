b930aac6ff133113e925c0149990117b4ae08e17
Eduardo Aparicio <aparicioe701@gmail.com>
2025-03-23 14:48:20 -0600

    juego de cartas llamado blackjack

---
+++ src/blackjack/usescases/valor-carta.js
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * @param {String} carta Obtener el valor de la carta
+ * @returns {Number} es el valor de la carta
+ */
+
+export const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
